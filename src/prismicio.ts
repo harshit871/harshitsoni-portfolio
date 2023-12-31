e1c799b1ea046d3a64f08e07b2fb8b85c16599e9
Harshit Soni <harshitashva@gmail.com>
2023-12-31 21:57:20 +0530

    feat: modified prismicio

--- src/prismicio.ts
+++ src/prismicio.ts
@@ -13,15 +13,24 @@ export const repositoryName =
  *
  * {@link https://prismic.io/docs/route-resolver#route-resolver}
  */
-// TODO: Update the routes array to match your project's route structure.
+
 const routes: prismic.ClientConfig["routes"] = [
+  // Examples:
   {
-  	type: "homepage",
-  	path: "/",
+    type: "homepage",
+    path: "/",
   },
   {
-  	type: "page",
-  	path: "/:uid",
+    type: "page",
+    path: "/:uid",
+  },
+  {
+    type: "blog_post",
+    path: "/blog/:uid",
+  },
+  {
+    type: "project",
+    path: "/project/:uid",
   },
 ];
 
