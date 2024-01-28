48238d54fcdb5d8a24a04296786e27e8af0dca2b
Harshit Soni <harshitashva@gmail.com>
2024-01-28 19:48:01 +0530

    fix

--- src/slices/Hero/index.tsx
+++ src/slices/Hero/index.tsx
@@ -77,6 +77,7 @@ const Hero = ({ slice }: HeroProps): JSX.Element => {
     >
       <div className="grid min-h-[70vh] grid-cols-1 items-center md:grid-cols-2">
         <Shapes />
+        
         <div className="col-start-1 md:row-start-1 " data-speed=".2">
           <h1
             className="mb-8 text-[clamp(3rem,20vmin,20rem)] font-extrabold leading-none tracking-tighter"
