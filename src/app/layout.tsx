8ee4e6d97e2ac03c9ba8a6086724519003d1a2c7
Harshit Soni <harshitashva@gmail.com>
2024-01-31 23:42:09 +0530

    indentation

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,10 +32,15 @@ export default function RootLayout({
     <html lang="en" className="bg-slate-900">
       <body className={clsx(urbanist.className, "relative min-h-screen")}>
         <Header />
+
         {children}
+
         <div className="background-gradient absolute inset-0 -z-50 max-h-screen" />
+        
         <div className="pointer-events-none absolute inset-0 -z-40 h-full bg-[url('/noisetexture.jpg')] opacity-20 mix-blend-soft-light"></div>
+
         <Footer />
+
         <PrismicPreview repositoryName={repositoryName} />
       </body>
     </html>
