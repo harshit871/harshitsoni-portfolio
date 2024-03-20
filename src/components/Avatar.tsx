8bd7f0a69646add31f786e049fff4bab87f32653
Harshit Soni <harshitashva@gmail.com>
2024-03-21 00:14:39 +0530

    fix: Avatar Image

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -73,14 +73,13 @@ export default function Avatar({
   }, [prefersReducedMotion]);
 
   return (
-    <div ref={component} className={clsx("relative h-full w-full", className)}>
+    <div ref={component} className={clsx(" ", className)}>
       <div
         className="avatar aspect-square overflow-hidden rounded-3xl border-2 border-slate-700 opacity-0"
         style={{ perspective: "500px", perspectiveOrigin: "150% 150%" }}
       >
         <PrismicNextImage
           field={image}
-          className="avatar-image h-full w-full object-fill"
           imgixParams={{ q: 90 }}
         />
         <div className="highlight absolute inset-0 hidden w-full scale-110 bg-gradient-to-tr from-transparent via-white to-transparent opacity-0 md:block"></div>
