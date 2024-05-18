b45fea5feab8ffa65436a2967664ee582d10eee6
Harshit Soni <harshitashva@gmail.com>
2024-05-18 14:19:32 +0530

    fix: metadata changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,5 +18,21 @@ export async function generateMetadata(): Promise<Metadata> {
   return {
     title: page.data.meta_title,
     description: page.data.meta_description,
+    keywords: "Harshit Soni, Frontend Developer, Frontend Engineer, Software Engineer, React.js, Nextjs, Typescript, Javascript, node.js, GraphQL, Redux, Tailwind CSS, HTML5, CSS3, Vanilla JS, DOM Manipulation",
+    openGraph: {
+      title:  'Harshit Soni',
+      description: page.data.meta_description || '',
+      url: "https://harshitsoni-portfolio.vercel.app/",
+      type: "website",
+      images: page.data.meta_image?.url ?? '',
+    },
+    twitter: {
+      card: "summary_large_image",
+      site: "@yourtwitterhandle",
+      title:  'Harshit Soni',
+      description: page.data.meta_description || '',
+      images: page.data.meta_image?.url ?? '',
+    },
   };
 }
+
